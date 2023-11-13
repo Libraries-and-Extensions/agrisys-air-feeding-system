e84599ddfef8dd635d5ea3efed7db9d8844d8a3e
Marc Jensen <marcjensenvirklund@gmail.com>
2023-11-13 17:22:01 +0100

    Repository base setup

---
+++ AgrisysAirFeedingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AgrisysAirFeedingSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
