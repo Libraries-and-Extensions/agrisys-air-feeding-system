dc08809c9b3f8ede408a39ff39e785c345bf26da
Marc Jensen <marcjensenvirklund@gmail.com>
2023-11-29 11:32:54 +0100

    Added javascript module Added taghelper

---
+++ AgrisysAirFeedingSystem/wwwroot/js/utils/IteractionPreventer.js
@@ -0,0 +1,43 @@
+﻿document.querySelectorAll("[data-long-press]").forEach(function (element) {    
+    let timeoutID;
+    
+    let actionStr = element.dataset.longPress;
+    let action = actionStr !== "" ? actionStr : 1000;
+    
+    let timeout = parseInt(element.dataset.longPressTimeout);
+    timeout = !isNaN(timeout) ? timeout : 1000;
+    
+    if (element.dataset.preventClick === "true") {
+        element.addEventListener("click", function (e) {
+            e.preventDefault();
+        })
+    }
+    
+
+    element.addEventListener("mousedown", function (e) {
+        if (e.isComposing || e.keyCode === 229) {
+            return;
+        }
+        
+        timeoutID = setTimeout(function () {
+            location.assign(action);
+            
+        }, timeout);
+    })
+
+    element.addEventListener("mouseup", function (e) {
+        if (e.isComposing || e.keyCode === 229) {
+            return;
+        }      
+
+        clearTimeout(timeoutID);
+    })
+
+    element.addEventListener("mouseleave", (e) => {
+        if (e.isComposing || e.keyCode === 229) {
+            return;
+        }
+
+        clearTimeout(timeoutID);
+    });
+});
