dde3c2cf02cb38d20e294260dc032701f014afd3
Philip Olesen <nbphilip@gmail.com>
2023-11-29 09:40:21 +0100

    kitchen initial

---
+++ AgrisysAirFeedingSystem/Areas/Kitchen/Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AgrisysAirFeedingSystem.Areas.Kitchen.Pages;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
