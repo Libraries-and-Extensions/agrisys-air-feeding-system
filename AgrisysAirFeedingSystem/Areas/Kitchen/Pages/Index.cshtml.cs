3435920a018215834916fa12579ffc357d48039d
Philip Olesen <nbphilip@gmail.com>
2023-11-30 13:52:16 +0100

    dbcontext moved to .cs pagemodel

--- AgrisysAirFeedingSystem/Areas/Kitchen/Pages/Index.cshtml.cs
+++ AgrisysAirFeedingSystem/Areas/Kitchen/Pages/Index.cshtml.cs
@@ -1,11 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using AgrisysAirFeedingSystem.Models.DB;
+using AgrisysAirFeedingSystem.Models.DBModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AgrisysAirFeedingSystem.Areas.Kitchen.Pages;
 
 public class Index : PageModel
 {
+    public DbSet<Silo> Silos;
     public void OnGet()
     {
-        
+        Silos = new AgrisysDbContext().Set<Silo>();
     }
 }
